f6de886ae29dfe66f0a5d32588ab6b4a20beb5de
leonelmontiel <leonel.montiel@outlook.com>
2026-05-01 21:11:35 -0300

    hasta barriles

--- tp-4/tp-4.hs
+++ tp-4/tp-4.hs
@@ -106,7 +106,7 @@ m1 = Fin c1
 m2 = Fin c2
 m3 = Bifurcacion c3 m0 m1
 m4 = Bifurcacion c4 m1 m2
-m5 = Bifurcacion c5 m1 m1
+m5 = Bifurcacion c5 m1 m4
 m6 = Bifurcacion c1 m0 m2
 m7 = Bifurcacion c1 m4 m4
 m8 = Bifurcacion c4 m7 m1
@@ -180,6 +180,7 @@ caminoDeLaRamaMasLarga (Bifurcacion _ mi md) =
 	if length(caminoDeLaRamaMasLarga mi) > length(caminoDeLaRamaMasLarga md)
 	then Izq : caminoDeLaRamaMasLarga mi
 	else Der : caminoDeLaRamaMasLarga md
+-----
 
 tesorosPorNivel :: Mapa -> [[Objeto]]
 {-Devuelve los tesoros separados por nivel en el árbol.
@@ -201,9 +202,179 @@ obtenerTesoros (ob:obs) =
 	if esTesoro ob
 	then ob : obtenerTesoros obs
 	else obtenerTesoros obs
+-----
+
+todosLosCaminos :: Mapa -> [[Dir]]
+{- Devuelve todos lo caminos en el mapa.
+Precondición: ninguna. -}
+todosLosCaminos (Fin _) = [[]]
+todosLosCaminos (Bifurcacion _ mi md) = agregarIzq (todosLosCaminos mi) ++ agregarDer (todosLosCaminos md)
+
+agregarIzq :: [[Dir]] -> [[Dir]]
+{- Dada una lista de caminos, agrega la dirección Izq al inicio de cada camino.
+Precondición: ninguna. -}
+agregarIzq [] = []
+agregarIzq (cam:cams) = (Izq : cam) : agregarIzq cams
 
 
+agregarDer :: [[Dir]] -> [[Dir]]
+{- Dada una lista de caminos, agrega la dirección Der al inicio de cada camino.
+Precondición: ninguna. -}
+agregarDer [] = []
+agregarDer (cam:cams) = (Der : cam) : agregarDer cams
+-----
 
+{- 3. Nave Espacial
+modelaremos una Nave como un tipo algebraico, el cual nos permite construir una nave espacial,
+dividida en sectores, a los cuales podemos asignar tripulantes y componentes. La representación
+es la siguiente:-}
+data Componente = LanzaTorpedos | Motor Int | Almacen [Barril]
+data Barril = Comida | Oxigeno | Torpedo | Combustible deriving Show
+data Sector = S SectorId [Componente] [Tripulante]
 
+type SectorId = String
+type Tripulante = String
 
+data Tree a = EmptyT | NodeT a (Tree a) (Tree a)
+data Nave = N (Tree Sector)
 
+b0 = []
+b1 = [Comida, Oxigeno]
+b2 = b1++[Torpedo, Oxigeno]
+
+s0 = S "0" [] []
+s1 = S "1" [LanzaTorpedos] []
+s2 = S "2" [Motor 8] ["Leo"]
+s3 = S "3" [Almacen b0] ["Leo", "Elias"]
+s4 = S "4" [Motor 20, Almacen b1] ["Leo", "Elias", "Montiel"]
+s5 = S "5" [Almacen b1, Almacen b2] ["Leo", "Elias", "Montiel"]
+
+ts0 = NodeT s0 EmptyT EmptyT
+ts1 = NodeT s1 ts0 EmptyT
+ts2 = NodeT s2 ts1 ts0
+ts3 = NodeT s3 ts1 ts2
+ts4 = NodeT s4 ts2 ts3
+ts5 = NodeT s5 ts3 ts3
+
+n0 = N (ts0)
+n1 = N (ts1)
+n2 = N (ts2)
+n3 = N (ts3)
+n4 = N (ts4)
+n5 = N (ts5)
+
+--Implementar las siguientes funciones utilizando recursión estructural:
+sectores :: Nave -> [SectorId]
+{-Propósito: Devuelve todos los sectores de la nave.
+Precondición: ninguna -}
+sectores (N ts) = sinRepetidos (sectoresIdDe ts)
+
+sectoresIdDe :: Tree Sector -> [SectorId]
+{- Dado un árbol de Sector, retorna una lista con todos los SectorId del árbol.
+Precondición: ninguna -}
+sectoresIdDe EmptyT = []
+sectoresIdDe (NodeT s tsi tsd) = sectorId s : (sectoresIdDe tsi ++ sectoresIdDe tsd)
+
+sectorId :: Sector -> SectorId
+{- Dado un Sector, retorna su respectivo SectorId.
+Precondición: ninguna. -}
+sectorId (S sid _ _) = sid
+
+sinRepetidos :: Eq a => [a] -> [a]
+{- Dada una lista de elementos, devuelve dicha lista sin sus elementos repetidos.
+Precondición: ninguna. -}
+sinRepetidos [] = []
+sinRepetidos (x:xs) =
+  if pertenece x xs
+    then sinRepetidos xs
+    else x : sinRepetidos xs
+
+pertenece :: Eq a => a-> [a]-> Bool
+{-Dados un elemento e y una lista xs devuelve True si existe un elemento en xs que sea igual
+a e.
+Precondición: ninguna. -}
+pertenece _ [] = False
+pertenece e (x:xs) = (e == x) || pertenece e xs
+-----
+
+poderDePropulsion :: Nave -> Int
+{- Propósito: Devuelve la suma de poder de propulsión de todos los motores de la nave. Nota:
+el poder de propulsión es el número que acompaña al constructor de motores.
+Precondición: ninguna. -}
+poderDePropulsion (N ts) = totalDePoderDePropulsion ts
+
+totalDePoderDePropulsion :: Tree Sector -> Int
+{- Dado un árbol de Sector, indica el poder total de propulsión que hay en todo el árbol.
+Precondición: ninguna. -}
+totalDePoderDePropulsion EmptyT = 0
+totalDePoderDePropulsion (NodeT s tsi tsd) = cantPoderDePropulsionDe s + totalDePoderDePropulsion tsi + totalDePoderDePropulsion tsd
+
+cantPoderDePropulsionDe :: Sector -> Int
+{- Dado un Sector, indica la cantidad de poder de propulsión que tiene tal sector.
+Precondición: ninguna. -}
+cantPoderDePropulsionDe (S _ comps _) = cantPoderDePropulsion comps
+
+cantPoderDePropulsion :: [Componente] -> Int
+{- Dada una lista de Componente, indica la cantidad de poder de propulsión que hay en ella.
+Precondición: ninguna. -}
+cantPoderDePropulsion [] = 0
+cantPoderDePropulsion (comp:comps) = cantPoderMotor comp + cantPoderDePropulsion comps
+
+cantPoderMotor :: Componente -> Int
+{- Dado un Componente, indica la cantidad de poder de propulsión que tiene si este fuera un Motor, sino retorna 0.
+Precondición: ninguna. -}
+cantPoderMotor (Motor n) = n
+cantPoderMotor _ = 0
+-----
+
+barriles :: Nave -> [Barril]
+{- Propósito: Devuelve todos los barriles de la nave.
+Precondición: ninguna. -}
+barriles (N ts) = barrilesEn ts
+
+barrilesEn :: Tree Sector -> [Barril]
+{- Dado un árbol de Sector, devuelve los barriles que existen en él, sin repetir.
+Precondición: ninguna. -}
+barrilesEn EmptyT = []
+barrilesEn (NodeT s tsi tsd) = barrilesDel s ++ barrilesEn tsi ++ barrilesEn tsd
+
+barrilesDel :: Sector -> [Barril]
+{- Dado un Sector, devuelve los barriles que existen en él, sin repetir.
+Precondición: ninguna. -}
+barrilesDel (S _ comps _) = obtenerBarriles comps
+
+obtenerBarriles :: [Componente] -> [Barril]
+{- Dada una lista de Componente, devuelve los barriles que existen en él, sin repetir.
+Precondición: ninguna. -}
+obtenerBarriles [] = []
+obtenerBarriles (comp:comps) = barrilesSinRepetir (barrilesSiEsAlmacen comp ++ obtenerBarriles comps)
+
+barrilesSiEsAlmacen :: Componente -> [Barril]
+{- Dado un Componente, devuelve los barriles que existen en él, sin repetir.
+Precondición: ninguna. -}
+barrilesSiEsAlmacen (Almacen bs) = bs
+barrilesSiEsAlmacen _ = []
+
+barrilesSinRepetir :: [Barril] -> [Barril]
+{- Dada una lista de Barril, devuelve los barriles que existen en él, sin repetir.
+Precondición: ninguna. -}
+barrilesSinRepetir [] = []
+barrilesSinRepetir (b:bs) =
+  if perteneceBarrilA b bs
+    then barrilesSinRepetir bs
+    else b : barrilesSinRepetir bs 
+
+perteneceBarrilA :: Barril -> [Barril] -> Bool
+{- Dado un Barril y una lista de Barril, indica si existe en ella.
+Precondición: ninguna. -}
+perteneceBarrilA _ [] = False
+perteneceBarrilA b (b':bs) = esMismoBarril b b' || perteneceBarrilA b bs
+
+esMismoBarril :: Barril -> Barril -> Bool
+{- Dado dos Barriles, indi a si son del mismo tipo.
+Precondición: ninguna. -}
+esMismoBarril Comida Comida = True
+esMismoBarril Oxigeno Oxigeno = True
+esMismoBarril Torpedo Torpedo = True
+esMismoBarril Combustible Combustible = True
+esMismoBarril _ _ = False
